b7141ed35e9d5f290da5333284ba3ecb9b0e4e14
Weifan Jiang <weifanjiang@D-10-157-87-16.dhcp4.washington.edu>
2018-02-18 22:17:12 -0800

    fix bugs

--- android/app/src/main/java/com/accessuw/MainApplication.java
+++ android/app/src/main/java/com/accessuw/MainApplication.java
@@ -3,7 +3,6 @@ package com.accessuw;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.zmxv.RNSound.RNSoundPackage;
 import com.RNPlayAudio.RNPlayAudioPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.yourbrandlive.RNNyt360Video.RNNyt360VideoViewPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSoundPackage(),
             new RNPlayAudioPackage(),
             new ReactVideoPackage(),
             new RNNyt360VideoViewPackage()
