30350f874e68e07e86e5560eb6c3ae6f8bb9b388
yuchong xiang <yvonna@D-69-91-187-2.dhcp4.washington.edu>
2018-02-27 14:33:48 -0800

    add phone call icon and create the view

---
+++ components/phonecall.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Image, TouchableOpacity } from 'react-native';
+import call from 'react-native-phone-call';
+
+export default class PhoneIcon extends React.Component {
+    render() {
+        const args = {
+            number: '0123456789',  // should be phone number of transit station
+        };
+
+        return (
+            <TouchableOpacity
+                onPress={
+                    () => call(args).catch(console.error)
+                }>
+                <Image source={require('../Icons/call.png')}/>
+            </TouchableOpacity>
+        );
+    }
+}
