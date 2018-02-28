19e3b99dedbeea25635c2a592a6db0240b885653
yuchong xiang <yvonna@yuchongs-MacBook-Pro.local>
2018-02-27 19:17:03 -0800

    add icons to the screen and adjust the layout

--- screens/startScreen.js
+++ screens/startScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Dimensions, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import PhoneIcon from '../components/phonecall';
+import InfoIcon from '../components/infomation';
 
 export default class StartScreen extends React.Component {
     static navigationOptions = {
@@ -10,6 +12,12 @@ export default class StartScreen extends React.Component {
 
         return (
             <View style={styles.container}>
+                <View style={{paddingLeft: width - 80}}>
+                    <View>
+                        <PhoneIcon/>
+                        <InfoIcon navigation={this.props.navigation}/>
+                    </View>
+                </View>
                 <Text style={styles.text}>Welcome to AccessUw APP</Text>
                 <TouchableOpacity
                     onPress={
@@ -24,29 +32,33 @@ export default class StartScreen extends React.Component {
     }
 }
 
+let { height, width } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        paddingTop: 80,
-        paddingLeft: 50,
-        paddingRight: 50,
+        paddingTop: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
         paddingBottom: 50,
     },
     text: {
         textAlign: 'center',
         fontSize: 28,
+        paddingLeft: 40,
+        paddingRight: 40,
         paddingBottom: 50,
     },
     button: {
-        width: 250,
-        height: 350,
+        width: width - 80,
+        height: height - 450,
         justifyContent: 'center',
         alignItems: 'center',
     },
     title: {
         fontSize: 32,
         color: '#007AFF',
-    }
+    },
 });
